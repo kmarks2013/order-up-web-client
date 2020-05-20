039690237ec42bba9159bce35bb41bf4b2e4036d
kmarks2013 <kmarks2013@gmail.com>
2020-05-20 15:38:03 -0400

    adds search bar to home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import SearchBar from '../components/SearchBar'
 
 export default function Home() {
     return (
         <div className='home'>
+            <SearchBar/>
             <h1>Test</h1>
         </div>
     )
