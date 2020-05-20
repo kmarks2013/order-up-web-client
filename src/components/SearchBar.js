329be890510545c123d74c862dda2008175fa5cb
kmarks2013 <kmarks2013@gmail.com>
2020-05-20 16:00:47 -0400

    adds class names to searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,9 +9,10 @@ export default function SearchBar() {
     }
 
     return (
-        <div>
+        <div className="search-bar">
         {console.log("term ",searchTerm)}
             <input
+                className='search-input'
                 value={searchTerm}
                 name='searchTerm'
                 onChange={handleSearchTerm}
