695dbe4313086cda2a45a22d83f81fbd94d5b112
kmarks2013 <kmarks2013@gmail.com>
2020-05-20 15:38:21 -0400

    adds components folder

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+    state = {
+        term: ''
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
